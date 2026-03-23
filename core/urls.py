1a2b88729fba404e03a4ed70492efcacc9872869
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2026-03-23 10:43:51 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
+    path("", lambda request: HttpResponse("API is running")),
+    
     path("admin/", admin.site.urls),
     path("api/users/", include("app.users.urls")),
     path("api/events/", include("app.events.urls")),
