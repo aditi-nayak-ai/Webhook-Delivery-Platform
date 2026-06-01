5392fa887c5fb555c53e8e4cf059a68413b1e29a
Aditi Nayak <153495639+aditi-nayak-ai@users.noreply.github.com>
2026-06-01 18:06:03 +0530

    Update serializers.py

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from .models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "email", "role", "password"]
         extra_kwargs = {
-            "password": {"write_only": True}
+            "password": {"write_only": True},
+            "role": {"read_only": True},
         }
 
     def create(self, validated_data):
