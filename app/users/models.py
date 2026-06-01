b958472bc90ffc3b74eff4909411a19ec725f759
Aditi Nayak <153495639+aditi-nayak-ai@users.noreply.github.com>
2026-06-01 18:05:23 +0530

    Update models.py

--- app/users/models.py
+++ app/users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     ROLE_CHOICES = [
         ("admin", "Admin"),
@@ -11,10 +12,8 @@ class User(AbstractUser):
         max_length=20,
         choices=ROLE_CHOICES,
         default="developer",
-        db_index=True
+        db_index=True,
     )
-
-    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.username
