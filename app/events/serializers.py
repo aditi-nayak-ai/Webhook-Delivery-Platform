4534e23fa576ac589be6525d1d65bde8fbb3b054
Aditi Nayak <153495639+aditi-nayak-ai@users.noreply.github.com>
2026-06-01 18:05:00 +0530

    Update serializers.py

--- app/events/serializers.py
+++ app/events/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import Event
 
+
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = "__all__"
+        fields = ["id", "event_type", "payload", "created_at"]
+        read_only_fields = ["id", "created_at"]
