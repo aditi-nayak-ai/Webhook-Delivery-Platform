79170e206cd8b36a4af97c424df0bda655403a8b
Aditi Nayak <153495639+aditi-nayak-ai@users.noreply.github.com>
2026-06-01 18:29:49 +0530

    Update urls.py

--- app/webhooks/urls.py
+++ app/webhooks/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import WebhookViewSet
- 
+
 router = DefaultRouter()
 router.register(r"", WebhookViewSet, basename="webhook")
- 
+
 urlpatterns = [
     path("", include(router.urls)),
 ]
- 
