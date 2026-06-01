780beac7e8f85d7515238d99484d6b09c81b0857
Aditi Nayak <153495639+aditi-nayak-ai@users.noreply.github.com>
2026-06-01 19:37:12 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import JsonResponse
-
-
-def home(request):
-    return JsonResponse({"status": "API is running"})
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", home),
+    path("", TemplateView.as_view(template_name="index.html")),
     path("admin/", admin.site.urls),
     path("api/users/", include("app.users.urls")),
     path("api/events/", include("app.events.urls")),
