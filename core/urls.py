6627662994bd32fec1777aa9fc6df1156c4bf4c4
Aditi Nayak <153495639+aditi-nayak-ai@users.noreply.github.com>
2026-06-07 17:48:51 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html")),
     path("admin/", admin.site.urls),
