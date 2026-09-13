108c89823d72790b6c938aa738abc991605f45c3
Aditi Nayak <153495639+aditi-nayak-ai@users.noreply.github.com>
2026-09-13 12:16:00 +0530

    Create admin.py

---
+++ app/events/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Event
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("id", "event_type", "created_at")
+    list_filter = ("event_type",)
+    readonly_fields = ("created_at",)
